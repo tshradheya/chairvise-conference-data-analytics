7b0ce7ded8d7782442e17a4e02b60427570de112
Codee Ong <codeeong@gmail.com>
2019-09-30 10:51:00 +0800

    add tests

---
+++ src/web/app/src/__tests__/DeleteModal.test.js
@@ -0,0 +1,31 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import DeleteModal from '@/components/common/DeleteModal.vue'
+
+let component, mockDeleteFn
+const localVue = createLocalVue()
+describe('DeleteModal.vue', () => {
+  beforeEach(()=> {
+    mockDeleteFn = jest.fn()
+    component = shallowMount(DeleteModal, {
+      propsData: {
+        typeOfDelete: 'presentation',
+        deleteFunction: mockDeleteFn
+    }
+    })
+  })
+
+  it('renders correct elements for presentation delete', () => {
+    expect(component.find('span').exists()).toBe(true)
+    expect(component.find('el-button').classes()).toContain('delete')
+    expect(component.find('el-button').attributes('icon')).toBe(undefined)
+  })
+
+  it('renders correct elements section delete', () => {
+    component.setProps({
+      typeOfDelete: 'section'
+    })
+    expect(component.props('typeOfDelete')).toBe('section')
+    expect(component.find('el-button').classes()).toContain('delete')
+    expect(component.find('el-button').attributes('icon')).toBe('el-icon-delete')
+  })
+})
