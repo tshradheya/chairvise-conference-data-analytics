595957a3d0193f5fa5438883ff047ac6ae68e833
Shradheya Thakre <tshradheya@gmail.com>
2019-10-17 01:45:58 +0800

    Add more migration

--- migrations/sql/20191014_add_conference_name.sql
+++ migrations/sql/20191014_add_conference_name.sql
@@ -6,4 +6,6 @@ ALTER TABLE review_record ADD conference_name varchar(255) DEFAULT 'defaultConf'
 ALTER TABLE submission_record ADD conference_name varchar(255) DEFAULT 'defaultConf';
 ALTER TABLE submission_author_record ADD conference_name varchar(255) DEFAULT 'defaultConf';
 
+ALTER TABLE presentation_section ADD conference_name varchar(255) default 'defaultConf';
+
 COMMIT;
